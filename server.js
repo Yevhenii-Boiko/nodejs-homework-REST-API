0613f240783586b560e28378138f572cf9dffc74
Yevhenii <boiko.yevgenii@gmail.com>
2023-05-21 21:46:30 +0300

    adds DB_HOST to config.js

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 const app = require("./app");
 
-const DB_HOST =
-  "mongodb+srv://Yevgenii:aoZIXQVf15VCuGPP@cluster0.rybsrh0.mongodb.net/db-contacts?retryWrites=true&w=majority";
+const { DB_HOST } = require("./config");
+
 mongoose
   .connect(DB_HOST)
   .then(() => {
