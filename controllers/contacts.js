384c8654b356947d54313ff9909cfda6823dbcaa
Yevhenii <boiko.yevgenii@gmail.com>
2023-06-01 16:22:18 +0300

    adds pagination

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,10 +4,12 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const result = await Contact.find({ owner }).populate(
-    "owner",
-    "email subscription"
-  );
+  const { page = 1, limit = 20 } = req.query;
+  const skip = (page - 1) * limit;
+  const result = await Contact.find({ owner }, "", {
+    skip,
+    limit: +limit,
+  }).populate("owner", "email subscription");
 
   res.json(result);
 };
