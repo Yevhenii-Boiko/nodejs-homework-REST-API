9d14f12b5264ee9aafc31c383687b03069b67fce
Yevhenii <boiko.yevgenii@gmail.com>
2023-06-01 16:34:42 +0300

    adds filter

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,14 +4,20 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAllContacts = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
   const result = await Contact.find({ owner }, "", {
     skip,
-    limit: +limit,
+    limit,
   }).populate("owner", "email subscription");
 
-  res.json(result);
+  if (favorite) {
+    res.json(
+      result.filter((contact) => contact.favorite.toString() === favorite)
+    );
+  } else {
+    res.json(result);
+  }
 };
 
 const getContactById = async (req, res) => {
