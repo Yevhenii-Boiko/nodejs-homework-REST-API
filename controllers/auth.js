fd151cbf04c3f8321ad7c39681797a50ccec3c41
Yevhenii <boiko.yevgenii@gmail.com>
2023-06-04 16:16:26 +0300

    adds jimp resizing

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const jwt = require("jsonwebtoken");
 const gravatar = require("gravatar");
 const path = require("path");
 const fs = require("fs/promises");
+const Jimp = require("jimp");
 
 const { User } = require("../models/user");
 
@@ -89,12 +90,20 @@ const updateSubscription = async (req, res) => {
 
 const updateAvatar = async (req, res) => {
   const { _id } = req.user;
+
   const { path: tmpUpload, originalname } = req.file;
+
+  const image = await Jimp.read(tmpUpload);
+  image.resize(250, 250);
+  await image.writeAsync(tmpUpload);
+
   const filename = `${_id}_${originalname}`;
   const resultUpload = path.join(avatarsDir, filename);
+
   await fs.rename(tmpUpload, resultUpload);
+
   const avatarURL = path.join("avatars", filename);
-  await User.findByIdAndUpdate(_id, { avatarURL });
+  await User.findByIdAndUpdate(_id, { avatarURL }, { new: true });
 
   res.json({
     avatarURL,
