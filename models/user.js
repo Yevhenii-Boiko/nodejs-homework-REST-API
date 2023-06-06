861693fac09136aab402e84a170c9b72fac50766
Yevhenii <boiko.yevgenii@gmail.com>
2023-06-06 22:08:34 +0300

    adds sg_package & updates schema

--- models/user.js
+++ models/user.js
@@ -31,6 +31,15 @@ const userSchema = new Schema(
       type: String,
       required: true,
     },
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      default: "",
+      required: [true, "Verify token is required"],
+    },
   },
   { versionKey: false, timestamps: false }
 );
